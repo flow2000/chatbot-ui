51da57012d1918eb8e46261ce671ceaef63b9055
flow2000 <1982989137@qq.com>
2023-05-09 10:58:58 +0800

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript, DocumentProps } from 'next/document';
+import i18nextConfig from '../next-i18next.config';
+
+type Props = DocumentProps & {
+  // add custom document props
+};
+
+export default function Document(props: Props) {
+  const currentLocale =
+    props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
+  return (
+    <Html lang={currentLocale}>
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="ChatGPT Web"></meta>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"K1uJEdmsTO3ETt8o",ck:"K1uJEdmsTO3ETt8o"});
+        `}} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
